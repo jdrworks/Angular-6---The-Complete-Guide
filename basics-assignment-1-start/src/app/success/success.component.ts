e087bbde3c3c7444952dac8ebbe2a9047103515d
Jeff Russell <sharf224@hotmail.com>
2018-06-24 23:17:40 -0400

    First Assignment

---
+++ basics-assignment-1-start/src/app/success/success.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-success-alert',
+	templateUrl: './success.component.html',
+	styleUrls: ['./success.component.css']
+})
+export class SuccessAlertComponent {
+}
